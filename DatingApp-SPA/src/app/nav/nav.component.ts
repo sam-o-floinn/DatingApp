06a9ee0217a0e6dd0279ee151c87b6699549cddb
Sam O'Floinn <samofloinn@protonmail.com>
2020-04-24 18:12:29 +0100

    End of Section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,12 +19,12 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in successfully');
     }, error => {
-      console.log('Failed to log in');
+      console.log(error);
     });
   }
   loggedIn() {
     const token = localStorage.getItem('token');
-    return !!token; //if there's something in this token variable, return true. if it's empty, return false
+    return !!token; // if there's something in this token variable, return true. if it's empty, return false
   }
 
   logout() {
