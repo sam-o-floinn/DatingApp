ff77c6436e6e2439ef1811acd0edb971619613b0
Sam O'Floinn <samofloinn@protonmail.com>
2020-05-13 17:26:46 +0100

    Pre-production 2

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -93,9 +93,13 @@ namespace DatingApp.API
 
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); //CORS authentication
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
